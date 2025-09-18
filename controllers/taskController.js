779d2c307b91843fc7c1d71b5ea20c1a09d3c649
Sanket Patel <sanpatel323@gmail.com>
2025-09-19 05:28:56 +0530

    api improvements

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -23,12 +23,31 @@ exports.createTask = async (req, res, next) => {
 // Get all tasks
 exports.getTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find({ user: req.user._id }).sort({ createdAt: -1 });
+    const { order = "asc" } = req.query; // default asc
+    const isAsc = order === "asc";
+
+    const tasks = await Task.aggregate([
+      { $match: { user: req.user._id } },
+      {
+        $addFields: {
+          sortDate: {
+            $ifNull: [
+              "$dueDate",
+              isAsc ? new Date("9999-12-31") : new Date("0001-01-01")
+            ]
+          }
+        }
+      },
+      { $sort: { sortDate: isAsc ? 1 : -1 } },
+      { $project: { sortDate: 0 } }
+    ]);
+
     res.json(tasks);
   } catch (err) {
     next(err);
   }
 };
+
 
 // Update task
 exports.updateTask = async (req, res, next) => {
